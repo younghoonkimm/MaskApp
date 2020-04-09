cc8f940857720c7bd424124340d8a8248b3ce0d4
younghoon <choosey_@naver.com>
2020-04-09 12:40:24 +0900

    Updates

---
+++ precache-manifest.4bf548297e837059913b8e8e4972ac78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1066dc14c97cba5b00ec2e36ab4511e4",
+    "url": "/younghoonKimm/MaskApp.git/index.html"
+  },
+  {
+    "revision": "cbbe786f491cff686383",
+    "url": "/younghoonKimm/MaskApp.git/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "9af3bd3a2fe8a43f7409",
+    "url": "/younghoonKimm/MaskApp.git/static/js/2.8d3328c3.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/younghoonKimm/MaskApp.git/static/js/2.8d3328c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbbe786f491cff686383",
+    "url": "/younghoonKimm/MaskApp.git/static/js/main.cc472a28.chunk.js"
+  },
+  {
+    "revision": "70d79177f8cc7f96a256",
+    "url": "/younghoonKimm/MaskApp.git/static/js/runtime-main.f40c01c9.js"
+  }
+]);
